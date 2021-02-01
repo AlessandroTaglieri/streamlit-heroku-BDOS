08b9663485200a56f1056702be98dd1bdb521854
Alessandro-Taglieri <alessandro.taglieri@ibm.com>
2021-02-01 01:18:35 +0100

    added model

--- pag5.py
+++ pag5.py
@@ -1,7 +1,26 @@
 import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
 def app():
 
     text = st.text_area('Insert here your violation descriptio to know the predcited risk category')
     st.write('You have inserted the following text: ', text)
+    if text != '':
+        with open('dict', "rb") as f:
+            vocabulary_to_load = pickle.load(f)
+            loaded_vectorizer = CountVectorizer(vocabulary=vocabulary_to_load)
+            loaded_vectorizer._validate_vocabulary()
+            st.write(loaded_vectorizer)
 
+            with open('risk_prediction.pkl', 'rb') as file:
+                model = pickle.load(file)
+
+                prediction = model.predict(loaded_vectorizer.transform(["ciao belli"]))  
+                if prediction == 1.0:
+                    output='Low Risk' 
+                elif prediction == 2.0:
+                    output='Moderate Risk' 
+                else:
+                    output='High Risk' 
+                st.write('Predicted RISK CATEGORY: ',output)
     
